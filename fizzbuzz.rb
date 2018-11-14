90d8f7e7bb4c377f66f24ce068bf7205244ced40
fukumoto <j1530@matsue-ct.jp>
2018-11-14 09:16:01 +0900

    add n for each print

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 for i in 1..100
     if i % 3 == 0 && i % 5 == 0
-        print "FizzBuzz"
+        print "FizzBuzz\n"
     elsif i % 3 == 0
-        print "Fizz"
+        print "Fizz\n"
     elsif i % 5 == 0
-        print "Buzz"
+        print "Buzz\n"
     elsif i % 7 == 0
-	print "git"
+	print "git\n"
     else
         p i
     end
